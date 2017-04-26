a795c7aafd659c3e2eece2c829caf4b686835bc9
Ty Nguyen T <developnguyen@gmail.com>
2017-04-26 17:38:39 +0700

    initial commit

---
+++ DemoCocoa/DemoCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  DemoCocoa
+//
+//  Created by Nguyen Thanh Ty on 4/26/17.
+//  Copyright © 2017 Nguyen Thanh Ty. All rights reserved.
+//
+
+import UIKit
+import Firebase
+import Alamofire
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DemoCocoa/Pods/Target Support Files/Pods-DemoCocoa/Pods-DemoCocoa-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_DemoCocoa : NSObject
+@end
+@implementation PodsDummy_Pods_DemoCocoa
+@end
